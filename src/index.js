2b6f16c86de50218273d1df8faa7817eb90542a7
Bakytzhan <bakytzhan90kz@gmail.com>
2019-03-15 07:45:30 +0600

    implement last zero counts

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  if(number===1) return 0;
+  
+  let ans = Number.MAX_SAFE_INTEGER
+  let primesOfBase = {}; 
+  primesOfBase = getPrimeFactorOfBase(base);
+  for(let k of Object.keys(primesOfBase)){
+    let count = Math.floor(sumOfPower(number, k) / primesOfBase[k]);
+    ans = Math.min(ans, count);
+  }
+
+  return ans;
 }
+
+
+function getPrimeFactorOfBase(num){
+  let primes = {};
+  for (let i = 2; num != 1; i++) { 
+    if (num % i == 0) { 
+        let count = 0; 
+        while (num % i == 0) { 
+            num = num / i; 
+            count++; 
+        } 
+
+        primes[i] = count; 
+    } 
+  } 
+  return primes; 
+}
+
+function sumOfPower(num, k) 
+{ 
+    let count = 0; 
+    let r = k; 
+    while (r <= num) {
+        count += Math.floor(num / r); 
+        r = r * k; 
+    } 
+    return count; 
+} 
